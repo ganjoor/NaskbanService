d92cb063a460af4eea5223c1dd1514b92c204dc9
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-04-04 20:06:11 +0330

    #4 GanjoorPoemMatchViewModel

---
+++ RMuseum/Models/PDFLibrary/ViewModels/GanjoorPoemMatchViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace RMuseum.Models.PDFLibrary.ViewModels
+{
+    public class GanjoorPoemMatchViewModel
+    {
+        /// <summary>
+        /// cat id
+        /// </summary>
+        public int GanjoorCatId { get; set; }
+
+        /// <summary>
+        /// start from this poem
+        /// </summary>
+        public int GanjoorPoemId { get; set; } = 0;
+
+        /// <summary>
+        /// book id
+        /// </summary>
+        public int BookId { get; set; }
+
+        /// <summary>
+        /// page number
+        /// </summary>
+        public int PageNumber { get; set; }
+
+        /// <summary>
+        /// threshold
+        /// </summary>
+        public int Threshold { get; set; }
+    }
+}
