2226e86d45ade6100ff091a566269103db613280
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-04-05 13:35:07 +0330

    #4 database migration

---
+++ RMuseum/Migrations/20240405100403_GanjoorMatchings.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class GanjoorMatchings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GanjoorPoemMatchFindings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GanjoorCatId = table.Column<int>(type: "int", nullable: false),
+                    GanjoorCatFullTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GanjoorCatFullUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GanjoorPoemId = table.Column<int>(type: "int", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    BookTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PageNumber = table.Column<int>(type: "int", nullable: false),
+                    Threshold = table.Column<int>(type: "int", nullable: false),
+                    QueueTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Started = table.Column<bool>(type: "bit", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastUpdate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastUpdatedByUserId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CurrentPoemId = table.Column<int>(type: "int", nullable: false),
+                    Progress = table.Column<int>(type: "int", nullable: false),
+                    CurrentPageNumber = table.Column<int>(type: "int", nullable: false),
+                    Finished = table.Column<bool>(type: "bit", nullable: false),
+                    FinishTime = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GanjoorPoemMatchFindings", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GanjoorPoemMatchFindings");
+        }
+    }
+}
