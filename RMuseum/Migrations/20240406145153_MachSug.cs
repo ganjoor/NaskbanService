1aca60bd7fac5de1fecb5d60f6e6fc36a3eebe2b
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-04-06 18:24:48 +0330

    machine suggested - db migration

---
+++ RMuseum/Migrations/20240406145153_MachSug.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class MachSug : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "SuggestedByMachine",
+                table: "PDFGanjoorLinks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SuggestedByMachine",
+                table: "PDFGanjoorLinks");
+        }
+    }
+}
