2299265703a8b1ae35422552c015943922f7c974
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-09-19 08:19:59 +0330

    db migration

---
+++ RMuseum/Migrations/20240919044934_VisitsIndex.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class VisitsIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PDFVisitRecords_RAppUserId",
+                table: "PDFVisitRecords");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PDFVisitRecords_RAppUserId_PDFBookId",
+                table: "PDFVisitRecords",
+                columns: new[] { "RAppUserId", "PDFBookId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PDFVisitRecords_RAppUserId_PDFBookId",
+                table: "PDFVisitRecords");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PDFVisitRecords_RAppUserId",
+                table: "PDFVisitRecords",
+                column: "RAppUserId");
+        }
+    }
+}
