a5449dacc6a289774f9c36dc533327966d8cdf17
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2024-12-08 14:31:49 +0330

    ai queue db migration

---
+++ RMuseum/Migrations/20241208110103_AIQ.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class AIQ : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AIRevised",
+                table: "PDFBooks",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "AIQueuedItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PDFBookId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AIQueuedItems", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AIQueuedItems");
+
+            migrationBuilder.DropColumn(
+                name: "AIRevised",
+                table: "PDFBooks");
+        }
+    }
+}
