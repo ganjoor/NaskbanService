97cb934b5ac6e24c138c5527112ff6a64c1cf407
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2026-08-23 09:52:09 +0330

    db migartion

---
+++ RMuseum/Migrations/20260823062150_AddPDFPageCommentEditedAt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RMuseum.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPDFPageCommentEditedAt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EditedAt",
+                table: "PDFPageComments",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EditedAt",
+                table: "PDFPageComments");
+        }
+    }
+}
